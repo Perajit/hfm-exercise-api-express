ccd9000ecda30584db8d3c475a52d9b5037542be
Perajti Pasupat <perajit.p@gmail.com>
2025-09-19 00:08:54 +0700

    chore: support deployment on vercel

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,6 @@
-import bodyParser from 'body-parser';
-import cookieParser from 'cookie-parser';
-import cors, { CorsOptions } from 'cors';
-import 'dotenv/config';
-import express from 'express';
-import morgan from 'morgan';
-import { logger } from './logger';
-import authRouter from './modules/auth/auth.router';
-import userRouter from './modules/user/user.router';
+import app from './app';
 
-const app = express();
-const port = 3000;
-const corsOrigin = process.env.CLIENT_APP_ORIGIN;
-const corsOptions: CorsOptions = {
-  origin: corsOrigin,
-  preflightContinue: true,
-  methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
-};
-const corsOptionsWithCredential: CorsOptions = {
-  ...corsOptions,
-  credentials: true,
-};
-
-app.options('*', cors(corsOptions));
-userRouter.use(cors(corsOptions));
-app.use(cors(corsOptionsWithCredential));
-
-// For requests / response processing
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
-
-// For logging
-app.use(morgan('combined', {
-  stream: {
-    write: text => logger.info(text),
-  },
-}));
-
-// For routes
-app.use('/user', userRouter);
-app.use('/auth', authRouter);
+const port = process.env.APP_PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Application is running on port ${port}`);
